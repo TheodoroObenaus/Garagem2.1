f4288ba14a07f1c80c417b7fd20217a6e05d36e0
TheodoroObenaus <theodoro.obenaus@gmail.com>
2024-07-23 15:15:52 -0300

    Api categoria

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet ,AcessorioViewSet 
+from core.views import UserViewSet ,AcessorioViewSet ,CategoriaViewSet
 
 router = DefaultRouter()
 
 router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
